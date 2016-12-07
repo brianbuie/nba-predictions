ade470cabd720fe29f55018224cd1249cf9bde0a
buoybuoy <brianjamesbuie@gmail.com>
2016-12-06 19:09:31 -0600

    display api

---
+++ display_api.php
@@ -0,0 +1,11 @@
+<?php include('api.php');
+
+echo '<pre>';
+echo '-----------------------WEST';
+var_dump($westStandings);
+echo '-----------------------EAST';
+var_dump($eastStandings);
+echo '</pre>';
+
+
+?>
