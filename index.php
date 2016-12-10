b7076b3e9e9778bbee1874caea4f7a651a496875
buoybuoy <brianjamesbuie@gmail.com>
2016-12-10 17:31:20 -0600

    css cache prevention

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ foreach($entries as $user => $files){
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>NBA Predictions</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style2.css" type="text/css">
+	<link rel="stylesheet" href="css/style.css?<?php echo rand(1, 1234423); ?>" type="text/css">
 </head>
 
 <body>
