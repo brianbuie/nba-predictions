10b580e61dd5b9deb8a279beac1d57d63850b67a
buoybuoy <brianjamesbuie@gmail.com>
2016-12-17 21:14:22 -0600

    edited Api to return user picks as array instead of number-keyed object

--- api/classes/user.class.php
+++ api/classes/user.class.php
@@ -8,11 +8,10 @@ class User {
 
 	function __construct($name, $picks){
 		$this->name = $name;
-		$this->picks = $picks;
-		foreach($this->picks as $conference => $teams){
-			foreach($teams as $rank => $team){
-				$values = ['rank' => $rank];
-				$this->set_pick_info($team['team'], $values);
+		foreach($picks as $conference => $teams){
+			foreach($teams as $rank => $pick){
+				$pick['rank'] = $rank;
+				$this->picks[$conference][] = $pick;
 			}
 		}
 		$images = json_decode(file_get_contents('data/images.json'), true);
@@ -20,10 +19,6 @@ class User {
 			$this->img = end($images[$this->name]);
 		}
 	}
-
-	// public function get_conference_picks($conference){
-	// 	return $this->picks[$conference];
-	// }
 
 	public function set_pick_info($team_id, $values_array){
 		foreach($this->picks as $conference => $teams){
