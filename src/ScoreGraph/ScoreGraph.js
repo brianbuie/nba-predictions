986f8a490e85b86b5d8ed79560c80188a3ebe1ac
buoybuoy <brianjamesbuie@gmail.com>
2016-12-25 22:12:30 -0600

    updated chartjs to 2, minimalist scoregraph working

--- src/ScoreGraph/ScoreGraph.js
+++ src/ScoreGraph/ScoreGraph.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './ScoreGraph.css';
-import { Line } from 'react-chartjs';
+import { Line } from 'react-chartjs-2';
 
 class ScoreGraph extends Component {
 
 	render() {
 		return (
 			<div className="row">
-				<div className="col-xs-12">
+				<div className="col-xs-12 no-padding">
 					{ this.createChart() }
 				</div>
 			</div>
@@ -20,6 +20,7 @@ class ScoreGraph extends Component {
 				labels: this.props.allScores[0].data.map( date => { return date.label; }),
 				datasets: this.props.allScores.map( user => {
 					return {
+						borderColor: '#fff',
 						label: user.label,
 						data: user.data.map( entry => { return entry.y; }),
 						fill: false,
@@ -30,7 +31,19 @@ class ScoreGraph extends Component {
 			}} 
 			options={{
 				responsive: true,
-
+				onClick: (e) => { console.log(e); },
+				legend: {
+					display: false,
+				},
+				tooltips: {
+					enabled: true,
+					intersect: false
+				},
+				scales: {
+					xAxes: [{
+						display: false
+					}]
+				}
 			}}
 		/>);
 	}
